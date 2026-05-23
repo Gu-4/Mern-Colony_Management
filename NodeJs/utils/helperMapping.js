3ac7f23f14df6ae5fc16099f580643e4ee758a70
Gu-4 <gursimratk.mhgga@gmail.com>
2026-05-23 23:10:17 +0530

    Add files via upload

---
+++ NodeJs/utils/helperMapping.js
@@ -0,0 +1,13 @@
+const complaintToHelperMap = {
+  "Electrical Issue": "Electrician",
+  "Plumbing-Water Issue": "Plumber",
+  "Cleaning & Hygiene": "Housekeeping",
+  "Maintenance": "Technician",
+  Security: "Security Guard",
+  "Parking Issue": "Security Guard",
+  "Noise Complaint": "Security Guard",
+  "Appliances": "Technician",
+  "Cleaning & Hygiene":"Garbage Manager"
+};
+
+module.exports = complaintToHelperMap;
